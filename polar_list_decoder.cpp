67697d7e10a802b5290e658286df58b721e64e20
Yang Yang <yyang.ohiostate@gmail.com>
2018-03-17 16:50:53 -0700

    Add buildMemory and initializeMemory function

--- polar_list_decoder.cpp
+++ polar_list_decoder.cpp
@@ -1,2 +1,84 @@
 #include "polar_list_decoder.hpp"
 #include <vector>
+#include <cmath>
+#include <assert.h>
+
+
+inline double f(double a, double b)
+{
+    return ((a > 0) - (a < 0)) * ((b > 0) - (b < 0)) * std::min(std::abs(a), std::abs(b));
+}
+
+inline double g(double a, double b, int bit)
+{
+    return bit == 1 ? b - a : b + a;
+}
+
+PolarListDecoder::PolarListDecoder(int m, int L): m(m), L(L){
+    n = (1<<m);  
+    buildMemory();
+}
+
+void PolarListDecoder::buildMemory(void){
+    // construct P and B
+    for (int lambda = 0; lambda <= m; lambda++)
+    {
+        P.push_back(std::vector<std::vector<double> >());
+        B.push_back(std::vector<std::vector<std::vector<int> > >());
+        for(int s = 0; s < L; s++){
+            P[lambda].push_back(std::vector<double>(1 << (m - lambda), 0));
+            B[lambda].push_back(std::vector<std::vector<int> >(1 << (m - lambda), std::vector<int>(2)));
+        }
+    }
+    // construct inactivePathIndexStack
+    assert(inactivePathIndexStack.size()==0);
+    for(int s = L-1; s >= 0; s--){
+        inactivePathIndexStack.push(s);
+    }
+    // construct activePath bool array
+    activePath = std::vector<bool>(L, false);
+    // construct inactiveArrayIndexStack
+    inactiveArrayIndexStack = std::vector<std::stack<int> >(m, std::stack<int>());
+    for(int lambda = 0; lambda < m; lambda++){
+        for(int s = L-1; s >=0; s--){
+            inactiveArrayIndexStack[lambda].push(s);
+        }
+    }
+    // construct arrayReferenceCount of P and B
+    arrayReferenceCount = std::vector<std::vector<int> >(m, std::vector<int>(L, 0));
+    // construct path to B/P array index
+    pathToArrayIndex = std::vector<std::vector<int> >(m, std::vector<int>(L, 0));
+}
+
+void PolarListDecoder::initializeMemory(void){
+    while(inactivePathIndexStack.size()) inactivePathIndexStack.pop();
+    for(int list = L-1; list >= 0; list--){
+        inactivePathIndexStack.push(list);
+        activePath[list] = false;
+    }
+    for(int lambda = 0; lambda < m; lambda++){
+        while(inactiveArrayIndexStack[lambda].size()) inactiveArrayIndexStack[lambda].pop();
+        for(int s = L-1; s >=0; s--){
+            inactiveArrayIndexStack[lambda].push(s);
+            arrayReferenceCount[lambda][s] = 0;
+        }
+    }
+}
+
+int PolarListDecoder::getInitialPath(void){
+    // intialize memory
+    initializeMemory();
+    // get the initial path
+    int initialPathIndex = inactivePathIndexStack.top();
+    inactivePathIndexStack.pop();
+    // update activePath
+    activePath[initialPathIndex] = true;
+    // assign array to path
+    for(int lambda=0; lambda<m; lambda++){
+        int array_index = inactiveArrayIndexStack[lambda].top();
+        inactiveArrayIndexStack[lambda].pop();
+        pathToArrayIndex[lambda][initialPathIndex] = array_index;
+        arrayReferenceCount[lambda][array_index] = 1;
+    }
+    return initialPathIndex;
+}
--- polar_list_decoder.hpp
+++ polar_list_decoder.hpp
@@ -2,21 +2,47 @@
 #define POLAR_LIST_DECODER_HPP
 
 #include <vector>
+#include <stack>
 
 class PolarListDecoder{
     int m;
     int n;
+    int L;
+    // data structure: P[lambda][list][branch: 0-->1<<(m-lambda)]
     std::vector<std::vector<std::vector<double> > > P;
+    // data structure: B[lambda][list][branch: 0-->1<<(m-lambda)][parity]
     std::vector<std::vector<std::vector<std::vector<int> > > > B;
+    // data structure: stack recording inactive path index
+    std::stack<int> inactivePathIndexStack;
+    // data structure: size L array recording activate path index
+    std::vector<bool> activePath;
+    // data structure: size m vector of stack recroding inactive array index
+    std::vector<std::stack<int> > inactiveArrayIndexStack;
+    // data structure: count the number of references to an array
+    std::vector<std::vector<int> > arrayReferenceCount;
+    // data structure: register the mapping from path to array index
+    std::vector<std::vector<int> > pathToArrayIndex;
+
+    // initialize data structure
+    void buildMemory(void);
+    void initializeMemory(void);
+    int getInitialPath(void);
+    
+    // recursively update P for all active list
     void recursivelyCalcP(int lambda, int phi);
+    // recursively update B for all active list
     void recursivelyCalcB(int lambda, int phi);
+    
+    // getter and setter for P and B
     double Pget(int lambda, int list, int beta);
     double & Pset(int lambda, int list, int beta);
     int Bget(int lambda, int list, int beta, int parity);
     int & Bset(int lambda, int list, int beta, int parity);
 public:
-    PolarListDecoder(int m);
+    PolarListDecoder(int m, int L);
     std::vector<int> decode(std::vector<double> llr, std::vector<bool> info_mask);
 };
+
+
 
 #endif
